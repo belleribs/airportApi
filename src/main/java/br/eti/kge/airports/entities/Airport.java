6936311a9ea43d62d7f61823e415b809947b9e5a
belle <belle@gmail.com>
2025-03-26 08:26:54 -0300

    Término da Fase 2 –Implementado endpoint Airport -Add interface AirportRepository -Add classe AirportService -Add classe AirportController

--- src/main/java/br/eti/kge/airports/entities/Airport.java
+++ src/main/java/br/eti/kge/airports/entities/Airport.java
@@ -1,18 +1,47 @@
-package entities;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.eti.kge.airports.entities;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
-import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
-import jakarta.persistence.Column;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+/**
+ *
+ * @author sesideva
+ */
 @Entity
 @Table(name = "airport")
 public class Airport {
- @Id
- @GeneratedValue(strategy = GenerationType.IDENTITY)
- private long id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String name;
+    private String city;
+    private String country;
+
+    @Column(name = "iatacode")
+    private String iataCode;
+
+    @Column(name = "icaocode")
+    private String icaoCode;
+
+    private double latitude;
+    private double longitude;
+    private double altitude;
+
+    @Column(name = "offsetutc")
+    private double offsetFromUTC;
+
+    @Column(name = "dstcode")
+    private String dstCode;
+    private String timezone;
 
     public long getId() {
         return id;
@@ -109,24 +138,7 @@ public class Airport {
     public void setTimezone(String timezone) {
         this.timezone = timezone;
     }
- private String name;
- private String city;
- private String country;
+    
+    
 
- @Column(name = "iatacode")
- private String iataCode;
-
- @Column(name = "icaocode")
- private String icaoCode;
-
- private double latitude;
- private double longitude;
- private double altitude;
-
- @Column(name = "offsetutc")
- private double offsetFromUTC;
-
- @Column(name = "dstcode")
- private String dstCode;
- private String timezone;
 }
